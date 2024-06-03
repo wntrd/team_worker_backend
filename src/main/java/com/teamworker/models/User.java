cfa62963b038e1d46f7dac1838f933c0a92d74ca
wntrd <wntrdq@gmail.com>
2024-06-03 14:53:43 +0300

    create User class

---
+++ src/main/java/com/teamworker/models/User.java
@@ -0,0 +1,23 @@
+package com.teamworker.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+}
